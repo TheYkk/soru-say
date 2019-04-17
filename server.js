d2a336939b3c9112512917b288c327f10dde8202
Yusuf Kaan Karakaya <yusufkaan142@gmail.com>
2019-04-17 20:59:59 +0300

    small fix for express history

--- server.js
+++ server.js
@@ -3,5 +3,12 @@ const serveStatic = require("serve-static");
 const path = require("path");
 var app = express();
 app.use(serveStatic(path.join(__dirname, "dist")));
+app.use((req, res, next) => {
+  if (!req.originalUrl.includes("/dist/", 0)) {
+    res.sendFile(`${__dirname}/app/index.html`);
+  } else {
+    next();
+  }
+});
 const port = process.env.PORT || 80;
 app.listen(port);
